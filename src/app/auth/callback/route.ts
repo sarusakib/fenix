ec3c4e7a005acd40e6a974aa3079788a0f0864d9
sarusakib <sorowar2466@gmail.com>
2026-09-06 02:40:11 +0600

    Update route.ts

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { createClient } from '@/utils/supabase/server'
+import { createClient } from '../../../utils/supabase/server'
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
@@ -15,4 +15,4 @@ export async function GET(request: Request) {
   }
 
   return NextResponse.redirect(`${origin}/login?error=Could+not+authenticate+user`)
-      }
+}
