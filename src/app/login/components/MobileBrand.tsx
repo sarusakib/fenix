38fc8be95a2942c3b8bc9c013a661aa7273abe06
sarusakib <sorowar2466@gmail.com>
2026-09-07 08:34:10 +0600

    Create MobileBrand.tsx

---
+++ src/app/login/components/MobileBrand.tsx
@@ -0,0 +1,23 @@
+export default function MobileBrand() {
+  return (
+    <div className="mb-8 flex items-center justify-center lg:hidden">
+      <div className="flex items-center">
+        <div className="flex h-11 w-11 items-center justify-center rounded-xl border border-white/10 bg-white/[0.05]">
+          <span className="text-lg font-black">
+            F<span className="text-[#008080]">X</span>
+          </span>
+        </div>
+
+        <div className="ml-3">
+          <p className="text-lg font-black">
+            Feni<span className="text-[#008080]">X</span>
+          </p>
+
+          <p className="text-[9px] uppercase tracking-[0.25em] text-white/35">
+            Business Ecosystem
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
