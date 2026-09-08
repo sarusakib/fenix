2559462f13e359c254d0fde2a139d73783bf8ed1
sarusakib <sorowar2466@gmail.com>
2026-09-08 09:12:02 +0600

    Update middleware.ts

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -14,20 +14,33 @@ export async function updateSession(request: NextRequest) {
         getAll() {
           return request.cookies.getAll()
         },
-        setAll(cookiesToSet: Array<{ name: string; value: string; options?: any }>) {
-          cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
+
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(
+            ({ name, value }) => {
+              request.cookies.set(name, value)
+            }
+          )
+
           supabaseResponse = NextResponse.next({
             request,
           })
-          cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
+
+          cookiesToSet.forEach(
+            ({ name, value, options }) => {
+              supabaseResponse.cookies.set(
+                name,
+                value,
+                options
+              )
+            }
           )
         },
       },
     }
   )
 
-  await supabase.auth.getUser()
+  await supabase.auth.getClaims()
 
   return supabaseResponse
-            }
+}
