fc26b84c7f58191ac8eb068239c81b05650dd298
sarusakib <sorowar2466@gmail.com>
2026-09-08 09:00:52 +0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import AuthSync from '../components/AuthSync'
 import FenixFlow from './FenixFlow'
 
 import HomeThemeProvider from '../components/theme/HomeThemeProvider'
-import SiteBackground from '../components/SiteBackground'
+import SiteBackground from '../components/layout/SiteBackground'
 
 export const metadata = {
   title: 'FeniX | Business Ecosystem',
@@ -18,10 +18,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html
+      lang="en"
+      suppressHydrationWarning
+    >
       <body
         className="
           min-h-screen
+          w-full
           overflow-x-hidden
           bg-[#030506]
           text-white
@@ -33,7 +37,6 @@ export default function RootLayout({
         <FenixFlow>
           <HomeThemeProvider>
             <SiteBackground />
-
             {children}
           </HomeThemeProvider>
         </FenixFlow>
