1c793ac86d8a58ed7300bc6e667d866f7f3ff190
sarusakib <sorowar2466@gmail.com>
2026-09-08 21:10:21 +0600

    Update auth-utils.ts

--- src/lib/auth/auth-utils.ts
+++ src/lib/auth/auth-utils.ts
@@ -1,7 +1,6 @@
-import type { SupabaseClient } from '@supabase/supabase-js'
-import type { Database } from '@/types/database'
+import type { createClient } from '@/utils/supabase/client'
 
-type FenixSupabaseClient = SupabaseClient<Database>
+type FenixSupabaseClient = ReturnType<typeof createClient>
 
 export type AuthMode = 'login' | 'signup'
 
