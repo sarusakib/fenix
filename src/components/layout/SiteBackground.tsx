910e8efa0a3b6cd01154fca8aef0b25ff9c735a9
sarusakib <sorowar2466@gmail.com>
2026-09-08 08:18:34 +0600

    Update SiteBackground.tsx

--- src/components/layout/SiteBackground.tsx
+++ src/components/layout/SiteBackground.tsx
@@ -83,3 +83,78 @@ export default function SiteBackground() {
     </div>
   )
 }
+'use client'
+
+export default function SiteBackground() {
+  return (
+    <div
+      aria-hidden="true"
+      className="
+        pointer-events-none
+        fixed
+        inset-0
+        -z-50
+        overflow-hidden
+        bg-[#030506]
+      "
+    >
+      {/* PORTRAIT */}
+      <div
+        className="
+          fenix-portrait-background
+          absolute
+          inset-0
+          items-center
+          justify-center
+          bg-[#030506]
+        "
+        style={{
+          backgroundImage:
+            "url('/images/IMG_20260907_032431.png')",
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'contain',
+        }}
+      />
+
+      {/* LANDSCAPE */}
+      <div
+        className="
+          fenix-landscape-background
+          absolute
+          inset-0
+          items-center
+          justify-center
+          bg-[#030506]
+        "
+        style={{
+          backgroundImage:
+            "url('/images/fenix-login-desktop.png')",
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'contain',
+        }}
+      />
+
+      {/* Cinematic overlay */}
+      <div
+        className="
+          absolute
+          inset-0
+          bg-white/10
+          dark:bg-black/45
+        "
+      />
+
+      {/* Vignette */}
+      <div
+        className="
+          absolute
+          inset-0
+          bg-[radial-gradient(circle_at_center,transparent_20%,rgba(0,0,0,0.18)_100%)]
+          dark:bg-[radial-gradient(circle_at_center,transparent_15%,rgba(0,0,0,0.55)_100%)]
+        "
+      />
+    </div>
+  )
+}
