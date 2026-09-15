cb9388ce5649cfa428298c47fd1214dca7e7fa1b
sarusakib <sorowar2466@gmail.com>
2026-09-16 01:56:34 +0600

    Update middleware.ts

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,7 +1,9 @@
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
-export async function updateSession(request: NextRequest) {
+export async function updateSession(
+  request: NextRequest,
+) {
   let supabaseResponse = NextResponse.next({
     request,
   })
@@ -15,42 +17,37 @@ export async function updateSession(request: NextRequest) {
           return request.cookies.getAll()
         },
 
-        setAll(
-          cookiesToSet: Array<{
-            name: string
-            value: string
-            options?: {
-              domain?: string
-              encode?: (value: string) => string
-              expires?: Date
-              httpOnly?: boolean
-              maxAge?: number
-              path?: string
-              sameSite?: 'lax' | 'strict' | 'none'
-              secure?: boolean
-            }
-          }>
-        ) {
-          cookiesToSet.forEach(({ name, value }) => {
-            request.cookies.set(name, value)
-          })
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(
+            ({ name, value }) => {
+              request.cookies.set(
+                name,
+                value,
+              )
+            },
+          )
 
-          supabaseResponse = NextResponse.next({
-            request,
-          })
+          supabaseResponse =
+            NextResponse.next({
+              request,
+            })
 
           cookiesToSet.forEach(
-            ({ name, value, options }) => {
+            ({
+              name,
+              value,
+              options,
+            }) => {
               supabaseResponse.cookies.set(
                 name,
                 value,
-                options
+                options,
               )
-            }
+            },
           )
         },
       },
-    }
+    },
   )
 
   await supabase.auth.getClaims()
